9f1c7e11511af8914a526c907ce9726cecf87ead
SkepticalInhuman <96436121+SkepticalInhuman@users.noreply.github.com>
2024-06-21 14:16:41 +0530

    Update program2.py

--- Week 1 Python & Cryptography Assignment 2/program2.py
+++ Week 1 Python & Cryptography Assignment 2/program2.py
@@ -1,18 +1,18 @@
 def gcdExtended(a, b):
-    # Base Case
+    
     if a == 0:
         return b, 0, 1
 
     gcd, x1, y1 = gcdExtended(b % a, a)
 
-    # Update x and y using results of recursive call
+    
     x = y1 - (b // a) * x1
     y = x1
 
     return gcd, x, y
 
 def main():
-    # Prompt user for input
+    
     A = int(input("Enter value for A: "))
     B = int(input("Enter value for B: "))
     C = int(input("Enter value for C: "))
